d1bbbf0e8149c727b3991404e1f5a8cf97d668de
Interligo <interligo@yandex.ru>
2020-12-22 17:30:27 +0300

    Refactoring code: __init__ is added

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,23 +1,6 @@
-from selenium.webdriver.common.by import By
-
 from .base_page import BasePage
-from .login_page import LoginPage
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        link = self.browser.find_element_by_css_selector("#login_link")
-        link.click()
-        alert = self.browser.switch_to.alert
-        alert.accept()
-
-    def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, '#login_link_invalid'), 'Login link is not presented.'
-
-    def test_guest_can_go_to_login_page(browser):
-        link = "http://selenium1py.pythonanywhere.com"
-        page = MainPage(browser, link)
-        page.open()
-        page.go_to_login_page()
-        login_page = LoginPage(browser, browser.current_url)
-        login_page.should_be_login_page()
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
