3aac8cf3a316d13d1aeb2de311f3223bd8b1df9c
Interligo <interligo@yandex.ru>
2020-12-22 15:46:58 +0300

    Initial commit

---
+++ pages/product_page.py
@@ -0,0 +1,8 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
+        self.solve_quiz_and_get_code()
