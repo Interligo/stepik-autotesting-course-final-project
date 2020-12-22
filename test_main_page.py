dbd9b149e0165a514dddcfdb9381e9d740cbf4c2
Interligo <interligo@yandex.ru>
2020-12-22 16:22:40 +0300

    Code is moved to another file

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,4 @@
 from pages.main_page import MainPage
-from pages.product_page import ProductPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -14,10 +13,3 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
