1179da0515211ca67544ea4558440ab99781294f
Interligo <interligo@yandex.ru>
2020-12-22 17:28:34 +0300

    Registarion test is added

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,16 +3,22 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.REGISTER_FORM_EMAIL_FIELD).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_FORM_PASSWORD_FIELD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_FORM_PASSWORD_CONF_FIELD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_FORM_BUTTON).click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert 'login' in self.browser.current_url, 'Invalid login page url'
+        assert "login" in self.browser.current_url, "Invalid login page url"
 
     def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOG_IN_FORM), 'Login form is not presented'
+        assert self.is_element_present(*LoginPageLocators.LOG_IN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Register link is not presented'
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register link is not presented"
